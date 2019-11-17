32002c2d3bcae9bc79c0130ef52f134784737a91
kulinar310 <bullet_310@mail.ru>
2019-11-17 23:05:02 +0300

    feat: add profiles page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,9 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />{' '}
           <span className='hide-sm'>Dashboard</span>
@@ -24,6 +27,9 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
 
   const guestLinks = (
     <ul>
+      <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
       <li>
         <Link to='#!'>Developers</Link>
       </li>
